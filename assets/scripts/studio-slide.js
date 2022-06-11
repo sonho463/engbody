fc4976d84519bad66d348d6283bfb26f5ec352ae
Son Hong Su <son.ho463@gmail.com>
2022-06-11 14:14:58 +0900

    SPレウアウト終了

--- assets/scripts/studio-slide.js
+++ assets/scripts/studio-slide.js
@@ -1,6 +1,6 @@
 
 
-const swiper = new Swiper(".swiper", {
+const studioSwiper = new Swiper(".studio-swiper", {
 	// configure Swiper to use module
 
   // Optional parameters
@@ -16,6 +16,32 @@ const swiper = new Swiper(".swiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: ".swiper-scrollbar",
+  // },
+});
+const priceSwiper = new Swiper(".price-swiper", {
+
+	// configure Swiper to use module
+
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+	spaceBetween:20,
+	slidesPerView: 1.1,
+
+  // If we need pagination
+  pagination: {
+    el: ".price-swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".price-swiper-button-next",
+    prevEl: ".price-swiper-button-prev",
   },
 
   // And if we need scrollbar
